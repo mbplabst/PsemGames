4bd8c901eff42a2cf075b103e9a2e85024ab9bb8
Max Plabst <mbplabst05@gmail.com>
2022-12-16 00:16:32 +0100

    Improved Sound

--- games/tic-tac-toe/ttt-script.js
+++ games/tic-tac-toe/ttt-script.js
@@ -42,11 +42,14 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //AUDIO
     function playSound(audioName) {
-        //audio.loop == true;
         let audio = new Audio(audioName);
         audio.play();
     }
     
+    const toggleMusicButton = document.getElementById('toggle-music');
+    const backgroundMusicPlayer = document.getElementById('background-music');
+    toggleMusicButton.addEventListener('click', () => !backgroundMusicPlayer.paused ? backgroundMusicPlayer.pause() : backgroundMusicPlayer.play());
+
     // document.getElementById("background-music").addEventListener("click", displayDate);
 
     function displayDate() {
