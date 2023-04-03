1d466a5bd4be42b47a06ae69a883337dfcac7e83
Max Plabst <mbplabst05@gmail.com>
2023-04-03 03:34:21 +0200

    peop

---
+++ games/quizz/script.js
@@ -0,0 +1,9 @@
+// AUSGABE ÜBER HTML SCOREBOARD
+const questions_right = document.querySelector(null);
+const questions_streak = document.querySelector(null);
+const questions_wrong = document.querySelector(null);
+
+// WERTE DER EINZELNEN STATISTIKEN
+let statsRight = 0;
+let statsStreak = 0;
+let statsWrong = 0;
