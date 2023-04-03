8d9f683739f6b1b7c40ef4f98f4ec6bf13a3d66b
Max Plabst <mbplabst05@gmail.com>
2023-04-04 01:04:33 +0200

    -

--- games/quizz/script.js
+++ games/quizz/script.js
@@ -1,9 +1,49 @@
 // AUSGABE ÜBER HTML SCOREBOARD
-const questions_right = document.querySelector(null);
-const questions_streak = document.querySelector(null);
-const questions_wrong = document.querySelector(null);
+const questions_right = document.querySelector('.questions_right');
+const questions_streak = document.querySelector('.questions_streak');
+const questions_wrong = document.querySelector('.questions_wrong');
+
+const question = document.querySelector('.question');
+const category = document.querySelector('.category')
+const statistics = document.querySelector('.container-statistics');
+const questionContainer = document.querySelector('.grid-container');
+const startGameButton = document.querySelector('.start-game');
+const nextQuestionButton = document.querySelector('.next-question');
+
+const optionA = document.querySelector('.answer_A');
+const optionB = document.querySelector('.answer_B');
+const optionC = document.querySelector('.answer_C');
+const optionD = document.querySelector('.answer_D');
 
 // WERTE DER EINZELNEN STATISTIKEN
 let statsRight = 0;
 let statsStreak = 0;
 let statsWrong = 0;
+
+function startQuiz() {
+
+    startGameButton.classList.add("hide");
+
+    question.classList.remove('hide');
+    category.classList.remove('hide');
+    statistics.classList.remove('hide');
+    questionContainer.classList.remove('hide');
+
+}
+
+function checkQuestion() {
+
+}
+
+function selectAnswer() {
+    nextQuestionButton.classList.remove('hide');
+    // questionContainer.removeEventListener(selectAnswer());
+    questionContainer.removeEventListener("click", null);
+
+    questions_wrong.innerHTML = ++statsWrong;
+
+}
+
+function loadNewQuestion() {
+    nextQuestionButton.classList.add('hide');
+}
