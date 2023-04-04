f9bb9489d5745681a5fb1acbfa142f970c002d96
Max Plabst <mbplabst05@gmail.com>
2023-04-04 19:13:45 +0200

    dsadasd

--- games/quizz/script.js
+++ games/quizz/script.js
@@ -15,6 +15,12 @@ const optionB = document.querySelector('.answer_B');
 const optionC = document.querySelector('.answer_C');
 const optionD = document.querySelector('.answer_D');
 
+const allButtons = document.querySelectorAll('.wrapper-questions');
+
+let currentQuestion = null;
+let allQuestions = [];
+let answeredQuestions = [];
+
 // WERTE DER EINZELNEN STATISTIKEN
 let statsRight = 0;
 let statsStreak = 0;
@@ -22,7 +28,7 @@ let statsWrong = 0;
 
 /*********************** FUNKTIONEN ***********************/
 
-function startQuiz() {
+async function startQuiz() {
 
     startGameButton.classList.add("hide");
 
@@ -31,18 +37,59 @@ function startQuiz() {
     statistics.classList.remove('hide');
     questionContainer.classList.remove('hide');
 
+    await logJSONData();
+    loadNewQuestion();
+
 }
 
 function loadNewQuestion() {
     nextQuestionButton.classList.add('hide');
+
+    let index = Math.floor(Math.random() * allQuestions.length);
+    // console.log(index);
+
+    currentQuestion = allQuestions[index];
+    // console.log(currentQuestion);
+
+
+    question.innerText = currentQuestion.question;
+    category.innerHTML = currentQuestion.category;
+    optionA.innerText = currentQuestion.options[0];
+    optionB.innerText = currentQuestion.options[1];
+    optionC.innerText = currentQuestion.options[2];
+    optionD.innerText = currentQuestion.options[3];
+
+    allButtons.forEach(b => b.disabled = false);
+    allButtons.forEach(b => b.classList.remove('answer-correct', 'answer-wrong'));
 }
 
 
-function showQuestion() {
+async function logJSONData() {
+    const response = await fetch("questions.json");
+    const jsonData = await response.json();
 
+    allQuestions = jsonData;
+
+    // console.log(jsonData);
 }
 
-
-function selectAnswer() {
+function selectAnswer(button, index) {
     nextQuestionButton.classList.remove('hide');
+
+    allButtons.forEach(b => b.disabled = true);
+
+    if (currentQuestion.answer === button.innerText) {
+        console.log("richtig");
+        button.classList.add('answer-correct')
+    } else {
+        console.log("falsch");
+        button.classList.add('answer-wrong')
+    }
+
+    // console.log(index);
+
 }
+
+allButtons.forEach((button, index) => {
+    button.addEventListener('click', () => selectAnswer(button, index));
+});
